04f5b13075998ee86e50d92134cbbbd730f19249
LeptonProjects <leptonofficialprojects@gmail.com>
2023-10-17 02:24:54 -0700

    second commit -- @edwin

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shaila_rani_website/view/colors/colors.dart';
-import 'package:shaila_rani_website/view/fonts/google_poppins.dart';
 import 'package:shaila_rani_website/view/home/app_bar/app_bar.dart';
 import 'package:shaila_rani_website/view/home/footer/footer.dart';
 import 'package:shaila_rani_website/view/home/footer/widgets/copyright_widget.dart';
 import 'package:shaila_rani_website/view/home/widgets/HomeMainImage_widget/homeImage_section_widget.dart';
 import 'package:shaila_rani_website/view/home/widgets/details_contact_container_widget/contact_details_widget.dart';
+import 'package:shaila_rani_website/view/home/widgets/our_practices_widget/our_pratices_widget.dart';
+import 'package:shaila_rani_website/view/home/widgets/our_team_container_widget/our_Team_container.dart';
 import 'package:shaila_rani_website/view/home/widgets/shaila_rani_ReadMore_widger/shaila_rani_ReadMore.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -23,181 +24,13 @@ class HomeScreen extends StatelessWidget {
           child: ResponsiveMobileAppBar()),
       body: ListView(
         children: [
-          HomeMainImageScreenWidget(screenSize: screenSize), /// Main Image Screen Section >>>>>>>>>>>>>>>
+          HomeMainImageScreenWidget(screenSize: screenSize),  /// Main Image Screen Section >>>>>>>>>>>>>>>
 
           const ShailaRaniReadMoreContainerWidget(), // Read More Section >>>>>>>>>
-          ContactAndDeatilsContainerWidget(screenSize: screenSize),
-          Center(
-            child: SizedBox(
-              height: 80,
-              width: 200,
-              child: Center(
-                child: GooglePoppinsWidgets(
-                  text: 'OUR PRACTICES',
-                  fontsize: 25,
-                  fontWeight: FontWeight.w800,
-                  color: cWhite,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              top: screenSize.width / 25,
-              bottom: screenSize.width / 25,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SquareContainer(
-                  squareContainerHeading: "Domestic Violence",
-                  squareContainerbody:
-                      "Domestic violence makes no distinctions. It affects people of all sexes, ages, and socioeconomic backgrounds. The home should be a place of safety, not dread and violence. Don't suffer in silence.",
-                  imageUrl: 'assests/images/dom_violance.jpg',
-                ),
-                SizedBox(
-                  width: screenSize.width / 20,
-                ),
-                const SquareContainer(
-                  squareContainerHeading: "Contested Divorce",
-                  squareContainerbody:
-                      "Contested divorce is about more than simply defending your rights; it's also about building a brighter future for you. Keep your eyes on that goal and remain focused.",
-                  imageUrl: 'assests/images/divorce.jpg',
-                ),
-                SizedBox(
-                  width: screenSize.width / 20,
-                ),
-                const SquareContainer(
-                  squareContainerHeading: "Child Custody",
-                  squareContainerbody:
-                      "Child custody is much more than just legal rights; it's about providing your child with a secure and supportive environment. Maintain your focus to get the finest results, and remember that you are not alone.",
-                  imageUrl: 'assests/images/child_custody.jpg',
-                ),
-                SizedBox(
-                  width: screenSize.width / 20,
-                ),
-                const SquareContainer(
-                  squareContainerHeading: "Marriage",
-                  squareContainerbody:
-                      "We help couples from start to finish in their quest for court marriage-from preparing notice of marriage to file for an order for police protection.",
-                  imageUrl: 'assests/images/marrage_pic.jpg',
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              top: screenSize.width / 25,
-              bottom: screenSize.width / 25,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SquareContainer(
-                  squareContainerHeading: "Family Court",
-                  squareContainerbody:
-                      "The appeal from family courts lies directly to the High Court within 30 days of passing the order or the judgement. Such The appeal can be on both question of law and question of fact. No appeal lies against an order which is passed with the consent of the parties",
-                  imageUrl: 'assests/images/family_court.jpeg',
-                ),
-                SizedBox(
-                  width: screenSize.width / 20,
-                ),
-                const SquareContainer(
-                  squareContainerHeading: "Mediation",
-                  squareContainerbody:
-                      "Mediation is an excellent choice for matrimonial disputes, especially those involving child custody, support, and other issues. Section 914 of the Family Courts Act allows the Family Court to make fair efforts to settle matrimonial disputes, and Counselors' assist the Family Court in this effort.",
-                  imageUrl: 'assests/images/mediation.jpg',
-                ),
-                SizedBox(
-                  width: screenSize.width / 20,
-                ),
-                const SquareContainer(
-                  squareContainerHeading: "Maintanance",
-                  squareContainerbody:
-                      "The couple must decide by the agreement whether maintenance will be paid to or received by any spouse in case of a divorce by mutual consent. In these situations, the husband may give alimony or maintenance to the wife or vice versa, depending on the agreement. By the couple's contract, the court grants a divorce.",
-                  imageUrl: 'assests/images/maintanance.jpg',
-                ),
-                SizedBox(
-                  width: screenSize.width / 20,
-                ),
-                const SquareContainer(
-                  squareContainerHeading: "Family Councelling",
-                  squareContainerbody:
-                      "Family counselling is a type of social work that helps families address and resolve issues. It can be useful for couples who are struggling, or for families with children who are experiencing behavioural or emotional problems. Family counselling can also help to improve communication within the family unit.",
-                  imageUrl: 'assests/images/family_counceling.jpg',
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            //................................. OUR TEAM
-            height: 400,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                      height: 400,
-                      width: 600,
-                      child: Center(
-                        child: GooglePoppinsWidgets(
-                          text: "OUR TEAM",
-                          fontsize: 50,
-                          color: cWhite,
-                        ),
-                      )),
-                  Container(
-                    color: Colors.amber,
-                    height: 300,
-                    width: 01,
-                  ),
-                  SizedBox(
-                      height: 400,
-                      width: 600,
-                      child: Image.asset(
-                        'assests/images/group_photo.jpeg',
-                        fit: BoxFit.fill,
-                      )),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            // color: Colors.black,
-            height: 500,
-            // width: 600,
-            child: ListView.separated(
-                physics: const AlwaysScrollableScrollPhysics(),
-                // shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                controller: sscrollcontroller,
-                itemBuilder: (context, index) {
-                  return SizedBox(
-                    height: 400,
-                    width: 400,
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          persionPhotos[index],
-                          fit: BoxFit.cover,
-                        ),
-                        GooglePoppinsWidgets(
-                          text: "",
-                          fontsize: 15,
-                          fontWeight: FontWeight.bold,
-                        )
-                      ],
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    width: 20,
-                  );
-                },
-                itemCount: persionPhotos.length),
-          ),
+          // ContactAndDeatilsContainerWidget(screenSize: screenSize),
+          const OurPracticesContainerWidget(),
+          OurTeamContainerWidget(sscrollcontroller: sscrollcontroller),
+
           const Divider(
             height: 01,
             // thickness: 01,
@@ -216,12 +49,6 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-
-
-
-
-
-
 List<String> persionPhotos = [
   'assests/images/persion_4.jpeg',
   'assests/images/persion_2.jpeg',
@@ -235,77 +62,3 @@ List<String> headerText = [
   'About',
   'Sevices',
 ];
-
-class SquareContainer extends StatelessWidget {
-  final String imageUrl;
-  final String squareContainerHeading;
-  final String squareContainerbody;
-  const SquareContainer(
-      {super.key,
-      required this.imageUrl,
-      required this.squareContainerHeading,
-      required this.squareContainerbody});
-
-  @override
-  Widget build(BuildContext context) {
-    var screenSizeWidth = MediaQuery.of(context).size.width;
-    // var screeSizeHeight = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: const EdgeInsets.only(top: 0),
-      child: Container(
-          width: screenSizeWidth / 5,
-          height: screenSizeWidth / 3.5,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(0),
-            border: Border.all(width: 1, color: Colors.transparent),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black38,
-                offset: Offset(
-                  10.0,
-                  2.0,
-                ),
-                blurRadius: 10.0,
-                spreadRadius: 2.0,
-              ), //BoxShadow
-              BoxShadow(
-                color: Colors.black38,
-                offset: Offset(5.0, 5.0),
-                blurRadius: 10.0,
-                spreadRadius: 2.0,
-              ), //BoxShadow
-            ],
-          ),
-          child: Column(
-            children: [
-              Image.asset(imageUrl),
-              Center(
-                  child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 20, right: 20, top: 20, bottom: 10.0),
-                child: GooglePoppinsWidgets(
-                  text: squareContainerHeading,
-                  fontsize: screenSizeWidth / 70,
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-              const SizedBox(
-                height: 0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 100,
-                  child: GooglePoppinsWidgets(
-                    text: squareContainerbody,
-                    fontsize: screenSizeWidth / 115,
-                  ),
-                ),
-              ),
-            ],
-          )),
-    );
-  }
-}
---
+++ lib/view/home/widgets/our_team_container_widget/our_Team_container.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:shaila_rani_website/view/colors/colors.dart';
+import 'package:shaila_rani_website/view/fonts/google_poppins.dart';
+import 'package:shaila_rani_website/view/widgets/responsive/responsive.dart';
+
+import '../../home_screen.dart';
+
+class OurTeamContainerWidget extends StatelessWidget {
+  const OurTeamContainerWidget({
+    super.key,
+    required this.sscrollcontroller,
+  });
+
+  final ScrollController sscrollcontroller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            //................................. OUR TEAM
+            height: 280,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: ResponsiveWebSite.isMobile(context)
+                  ? Column(
+                      children: [
+                        SizedBox(
+                            height: 50,
+                            width:
+                                ResponsiveWebSite.isTablet(context) ? 200 : 300,
+                            child: Column(
+                              children: [
+                                Center(
+                                  child: GooglePoppinsWidgets(
+                                    text: "O U R  T E A M",
+                                    fontsize: 20,
+                                    color: cWhite,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 05),
+                                  child: Container(
+                                    color: Colors.amber,
+                                    height: 02,
+                                    width: 80,
+                                  ),
+                                ),
+                              ],
+                            )),
+                        SizedBox(
+                            height: 200,
+                            width: double.infinity,
+                            child: Image.asset(
+                              'assests/images/group_photo.jpeg',
+                              fit: BoxFit.cover,
+                            ))
+                      ],
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                            height: 400,
+                            width:
+                                ResponsiveWebSite.isTablet(context) ? 200 : 300,
+                            child: Center(
+                              child: GooglePoppinsWidgets(
+                                text: "OUR TEAM",
+                                fontsize: ResponsiveWebSite.isTablet(context)
+                                    ? 30
+                                    : 50,
+                                color: cWhite,
+                              ),
+                            )),
+                        Container(
+                          color: Colors.amber,
+                          height: 300,
+                          width: 01,
+                        ),
+                        SizedBox(
+                            height:
+                                ResponsiveWebSite.isTablet(context) ? 280 :600 ,
+                            width:
+                                ResponsiveWebSite.isTablet(context) ? 450 : 600,
+                            child: Image.asset(
+                              'assests/images/group_photo.jpeg',
+                              fit: BoxFit.cover,
+                            )),
+                      ],
+                    ),
+            ),
+          ),
+          SizedBox(
+              // color: Colors.black,
+              height: 500,
+              // width: 600,
+              child: ResponsiveWebSite.isDesktop(context)
+                  ? ListView.separated(
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      // shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      controller: sscrollcontroller,
+                      itemBuilder: (context, index) {
+                        return SizedBox(
+                          height: 400,
+                          width: 400,
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                persionPhotos[index],
+                                fit: BoxFit.cover,
+                              ),
+                              GooglePoppinsWidgets(
+                                text: "",
+                                fontsize: 15,
+                                fontWeight: FontWeight.bold,
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(
+                          width: 20,
+                        );
+                      },
+                      itemCount: persionPhotos.length)
+                  : GridView.count(
+                      crossAxisCount: 2,
+                      padding: const EdgeInsets.all(20),
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      primary: false,
+                      children: List.generate(
+                          persionPhotos.length,
+                          (index) => SizedBox(
+                                // margin: ,
+                                height: 100,
+                                width: 100,
+                                // color: Colors.amber,
+                                child: Column(
+                                  children: [
+                                    Image.asset(
+                                      persionPhotos[index],
+                                      fit: BoxFit.cover,
+                                    ),
+                                    GooglePoppinsWidgets(
+                                      text: "",
+                                      fontsize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    )
+                                  ],
+                                ),
+                              )),
+                    )),
+        ],
+      ),
+    );
+  }
+}
